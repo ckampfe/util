db7b076d6021432293a111d23c105ff680eff5ad
Clark Kampfe <clark.kampfe@gmail.com>
2023-06-26 23:43:52 -0500

    update traverse/3 test

--- lib/util.ex
+++ lib/util.ex
@@ -4,17 +4,26 @@ defmodule Util do
   """
 
   @doc """
-  example.nodes
-  |> List.first()
-  |> Traverser.traverse(fn
-  {_tag, _attrs, children} when is_list(children) -> true
-  l when is_list(l) -> true
-  _ -> false end,
-  fn
-    {_tag, _attrs, children} ->
-    children
-  end
-  )
+
+  iex> data = [{:body, [], [{:ul, [], [{:li, [], "hi"}, {:li, [], "there"}]}]}]
+  ...> data
+  ...> |> List.first()
+  ...> |> Util.traverse(
+  ...>   fn
+  ...>     {_tag, _attrs, children} when is_list(children) -> true
+  ...>     l when is_list(l) -> true
+  ...>     _ -> false
+  ...>   end,
+  ...>   fn
+  ...>     {_tag, _attrs, children} -> children end
+  ...> )
+  ...> |> Enum.to_list()
+  [
+    {:body, [], [{:ul, [], [{:li, [], "hi"}, {:li, [], "there"}]}]},
+    {:ul, [], [{:li, [], "hi"}, {:li, [], "there"}]},
+    {:li, [], "hi"},
+    {:li, [], "there"}
+  ]
   """
   def traverse(root, is_branch?, children) do
     walk = fn node, walker ->
--- test/util_test.exs
+++ test/util_test.exs
@@ -1,8 +1,4 @@
 defmodule UtilTest do
   use ExUnit.Case
   doctest Util
-
-  test "greets the world" do
-    assert Util.hello() == :world
-  end
 end
